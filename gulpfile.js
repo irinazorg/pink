79947d3ac07fb88768f6bd9cfd0fcef0d755e2ad
irinazorg <07ladymoon07@gmail.com>
2018-10-10 20:38:36 +0300

    Меню

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,29 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+var browserSync = require('browser-sync');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('less', function(){ // Создаем таск Less
+    return gulp.src('app/less/**/*.less') // Берем источник
+        .pipe(less()) // Преобразуем Less в CSS посредством gulp-sass
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+        .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+});
+
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+    browserSync({ // Выполняем browser Sync
+        server: { // Определяем параметры сервера
+            baseDir: 'app' // Директория для сервера - app
+        },
+        notify: false // Отключаем уведомления
+    });
+});
+
+gulp.task('watch', ['browser-sync', 'less'], function() {
+    gulp.watch('app/less/**/*.less', ['less']); // Наблюдение за sass файлами в папке sass
+    gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+});
