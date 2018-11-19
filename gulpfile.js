ee00c6fc9ae15ae6a32d2284ac64eb98d1d4c6d8
irinazorg <07ladymoon07@gmail.com>
2018-11-19 17:40:45 +0200

    добавка новых плагинов в джейсон

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
+"use strict";
+
 var gulp = require('gulp');
 var less = require('gulp-less');
 var browserSync = require('browser-sync');
 var autoprefixer = require('gulp-autoprefixer');
+var csso = require('gulp-csso');
+var imagemin = require("gulp-imagemin");
+var rename = require("gulp-rename");
 
 gulp.task('less', function(){ // Создаем таск Less
     return gulp.src('app/less/**/*.less') // Берем источник
@@ -11,7 +16,15 @@ gulp.task('less', function(){ // Создаем таск Less
             cascade: false
         }))
         .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+        .pipe(csso())
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('app/css'))
         .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+});
+
+
+gulp.task('images', function() {
+    return gulp.src('app/img/**/*.{png,jpg,svg}')
 });
 
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
@@ -23,7 +36,13 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
     });
 });
 
-gulp.task('watch', ['browser-sync', 'less'], function() {
-    gulp.watch('app/less/**/*.less', ['less']); // Наблюдение за sass файлами в папке sass
-    gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+gulp.task('watch', function() {
+
+    browserSync.init({
+        server: "app"
+    });
+
+    gulp.watch('app/less/**/*.less', ['less']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
 });
