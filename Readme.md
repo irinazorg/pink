b73915368af97cb7e8a1cc0bab8208442c7e9d09
irinazorg <07ladymoon07@gmail.com>
2019-02-25 17:00:35 +0200

    second commit

--- Readme.md
+++ Readme.md
@@ -58,3 +58,4 @@ git clone SSH-адрес_вашего_форка
 [travis-url]: https://travis-ci.org/htmlacademy-adaptive/60074-pink
 [dependency-image]: https://david-dm.org/htmlacademy-adaptive/60074-pink/dev-status.svg?style=flat-square
 [dependency-url]: https://david-dm.org/htmlacademy-adaptive/60074-pink?type=dev
+# pink
--- gulpfile.js
+++ gulpfile.js
@@ -4,28 +4,42 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var browserSync = require('browser-sync');
 var autoprefixer = require('gulp-autoprefixer');
-var csso = require('gulp-csso');
-var imagemin = require("gulp-imagemin");
-var rename = require("gulp-rename");
+// var csso = require('gulp-csso');
+// var imagemin = require("gulp-imagemin");
+var svgstore = require('gulp-svgstore');
+// var rename = require("gulp-rename");
 
 gulp.task('less', function(){ // Создаем таск Less
-    return gulp.src('app/less/**/*.less') // Берем источник
+    gulp.src('app/less/**/*.less') // Берем источник
         .pipe(less()) // Преобразуем Less в CSS посредством gulp-sass
         .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
+            browsers: ['last 2 versions', 'IE 11', 'Firefox ESR'],
             cascade: false
         }))
         .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
-        .pipe(csso())
-        .pipe(rename('style.min.css'))
-        .pipe(gulp.dest('app/css'))
+        // .pipe(csso())
+        // .pipe(rename("style.min.css"))
+        // .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
         .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
 
-
-gulp.task('images', function() {
-    return gulp.src('app/img/**/*.{png,jpg,svg}')
+gulp.task('svgstore', function () {
+    return gulp
+        .src('app/img/svg-sprite/*.svg')
+        .pipe(svgstore({ inlineSvg: true }))
+        .pipe(rename("sprite.svg"))
+        .pipe(gulp.dest('app/img'));
 });
+
+// gulp.task("images", function() {
+//   return gulp.src("img/**/*.{png,jpg,svg}")
+//     .pipe(imagemin([
+//         imagemin.optipng({optimizationLevel: 3}),
+//         imagemin.jpegtran({progressive: true}),
+//         imagemin.svgo()
+//     ]))
+//     .pipe(gulp.dest("app/img"));
+// });
 
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
     browserSync({ // Выполняем browser Sync
